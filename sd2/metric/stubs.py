ecfbd6d49e3ecab1d9ca7a282e4c1decf27a1198
Tom Mitchell <tmitchel@bbn.com>
2019-02-08 15:34:52 -0500

    Clean out dead code
    Remove obsolete code. Tweak the results to conform to CSV writing by
    implementing an iterator. Add a filter to component definitions.

--- sd2/metric/stubs.py
+++ sd2/metric/stubs.py
@@ -64,6 +64,7 @@ class ModuleDefinition(BaseDefinition):
 class SubTypeCounter:
 
     def __init__(self):
+        self.timestamp = None
         self.subtype = None
         self.stubs = 0
         self.filled_stubs = 0
@@ -72,7 +73,9 @@ class SubTypeCounter:
         self.filled_nonstubs = 0
         self.empty_nonstubs = 0
 
-    def add_component(self, thing):
+    def add_component(self, timestamp, thing):
+        if self.timestamp is None:
+            self.timestamp = timestamp
         if self.subtype is None:
             self.subtype = thing.subtype
         if thing.is_stub():
@@ -87,6 +90,16 @@ class SubTypeCounter:
                 self.filled_nonstubs += 1
             else:
                 self.empty_nonstubs += 1
+
+    def __iter__(self):
+        yield self.timestamp
+        yield self.subtype
+        yield self.stubs
+        yield self.filled_stubs
+        yield self.empty_stubs
+        yield self.nonstubs
+        yield self.filled_nonstubs
+        yield self.empty_nonstubs
 
 
 class StubsMetric(DataMetric):
@@ -126,6 +139,7 @@ class StubsMetric(DataMetric):
               OPTIONAL { ?type <http://sbols.org/v2#sequence> ?sequence }
               OPTIONAL { ?type <http://sbols.org/v2#component> ?component }
               OPTIONAL { ?type <http://sbols.org/v2#sequenceAnnotation> ?sequenceAnnotation }
+              FILTER (! strstarts(str(?subtype), "http://identifiers.org/so/SO:"))
           }
         '''
         result = self._query.fetch_SPARQL(self._query._server,
@@ -137,81 +151,13 @@ class StubsMetric(DataMetric):
         result = [ComponentDefinition(dbv) for dbv in result]
         return result
 
-    @property
-    def nonstub_modules(self):
-        # Could stubs be marked with #stub_object = "false"? If so, adjust the filter
-        sparql_query = '''SELECT ?s ?role
-          WHERE {
-              ?s <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://sbols.org/v2#ModuleDefinition> .
-              ?s <http://sbols.org/v2#role> ?role
-              OPTIONAL { ?s <http://sd2e.org#stub_object> ?o } .
-              FILTER (! bound(?o) && ! strstarts(str(?role), "http://www.openmath.org/cd/logic1"))
-          }
-          GROUP BY ?role
-        '''
-        result = self._query.fetch_SPARQL(self._query._server,
-                                          sparql_query)
-        result = self._query.format_query_result(result, ['s', 'role'])
-        return result
-
-    def module_is_filled(self, sd2_name, sd2_type):
-        sparql_query = '''SELECT ?rel
-          WHERE {
-              <%s> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://sbols.org/v2#ModuleDefinition> .
-              <%s> <http://sbols.org/v2#role> <%s> .
-              <%s> ?rel ?o
-              FILTER ( strstarts(str(?rel), "http://sbols.org/v2"))
-          }
-          GROUP BY ?type
-        '''
-        sparql_query = sparql_query % (sd2_name, sd2_name, sd2_type, sd2_name)
-        result = self._query.fetch_SPARQL(self._query._server,
-                                          sparql_query)
-        relations = self._query.format_query_result(result, ['rel'])
-        for relation in relations:
-            if relation.startswith('http://sbols.org/v2'):
-                pass
-            else:
-                logging.info('Skipping %s', relation)
-        return ('http://sbols.org/v2#module' in relations or
-                'http://sbols.org/v2#interaction' in relations or
-                'http://sbols.org/v2#functionalComponent' in relations)
-        return filled_count, empty_count
-
     def fetch(self):
-        modules = self.get_modules()
-        components = self.get_components()
-        rows = collections.defaultdict(SubTypeCounter)
-        for m in modules:
-            rows[m.subtype].add_component(m)
-        for c in components:
-            rows[c.subtype].add_component(c)
-        for st in sorted(rows.keys()):
-            row = rows[st]
-            logging.info('%s, %s, %s, %s, %s, %s, %s',
-                         st, row.stubs, row.filled_stubs, row.empty_stubs,
-                         row.nonstubs, row.filled_nonstubs, row.empty_nonstubs)
-        return []
-
-    def fetch_old(self):
-        result = []
         timestamp = int(time.time())
-        count_by_type = collections.defaultdict(int)
-        filled_by_type = collections.defaultdict(int)
-        for row in self.nonstub_modules:
-            sd2_name = row['s']
-            sd2_type = row['role']
-            count_by_type[sd2_type] += 1
-            if self.module_is_filled(sd2_name, sd2_type):
-                filled_by_type[sd2_type] += 1
-
-        # Add the CSV header
-        # Don't write the header each time, it ends up as a row in the
-        # middle when appending to the file.
-        # result.append(['Timestamp', 'URI', 'All', 'Filled', 'Empty'])
-        for sd2_type, count in count_by_type.items():
-            filled = filled_by_type[sd2_type]
-            empty = count - filled
-            result.append([timestamp, sd2_type, count, filled, empty])
-
-        return result
+        rows = collections.defaultdict(SubTypeCounter)
+        modules = self.get_modules()
+        for m in modules:
+            rows[m.subtype].add_component(timestamp, m)
+        components = self.get_components()
+        for c in components:
+            rows[c.subtype].add_component(timestamp, c)
+        return sorted(rows.values(), key=lambda x: x.subtype)
