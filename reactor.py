7caf65f9e74d8520312649977cb34d736da89efb
Tom Mitchell <tmitchel@bbn.com>
2019-02-27 14:24:38 -0500

    Get SynBioHub user/password via Reactor config/secrets

--- reactor.py
+++ reactor.py
@@ -1,11 +1,22 @@
+import json
+import os
+
 from reactors.runtime import Reactor
 
 
 def main():
-    """Main function"""
     r = Reactor()
     r.logger.info("Hello this is actor {}".format(r.uid))
+    md = r.context.message_dict
+    r.logger.info(json.dumps(md))
 
+    # SynBioHub settings are passed via secrets.json
+    r.logger.info('Getting synbiohub settings')
+    sbh_settings = r.settings.get('sbh', {})
+    sbh_user = sbh_settings['user']
+    sbh_password = sbh_settings['password']
+    r.logger.info('SynBioHub user: %s', sbh_user)
+    r.logger.info('SynBioHub password is available')
 
 if __name__ == '__main__':
     main()
